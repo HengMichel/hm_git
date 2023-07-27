0cf11c6ccfa96a834091ed2d84b52d0a7d76a121
heng michel <cavacmoi@gmail.com>
2023-07-27 10:52:23 +0200

    firstCommit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Form PHP</title>
+</head>
+<body>
+    <!-- <form action=""> -->
+
+    <h1>Formulaire exo PHP</h1>
+
+    <form id="monForm">
+        <fieldset>
+            <div class="ligne">
+              <label for=" firstName"></label>
+              <input type="text" id="firstName" placeholder="Your first name" />
+            </div>
+            <div class="ligne">
+              <label for=" lastName"></label>
+              <input type="text" id="lastName" placeholder="Your last name" />
+            </div>
+
+            <div class="ligne">
+              <label for=" email"></label>
+              <input type="email" id="email" placeholder="Your email" />
+            </div>
+            <button id="submit">Submit</button>        
+        </fieldset>
+    </form>
+
+    <!-- </form> -->
+
+        <!-- <label for=""></label>first name<input type="text">
+
+        <label for=""></label>last name<input type="text">
+
+        <label for=""></label>email<input type="text"> -->
+
+      
+
+
+
+   
+</body>
+</html>
